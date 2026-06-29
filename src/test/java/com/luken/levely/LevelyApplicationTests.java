e5fc6bc75c082e47bd9c2cd4f70cc8a8dc3a6aaa
lucasdevzx <silvalucas.me.contact@gmail.com>
2026-06-29 09:18:09 -0300

    Create project structure

---
+++ src/test/java/com/luken/levely/LevelyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luken.levely;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevelyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
